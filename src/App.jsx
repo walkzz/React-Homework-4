67d028b0f8078fe5cdb4493ae782cf39b046a40d
Pavel Toshinski <pavelce2000@hotmail.com>
2024-10-06 23:04:01 +0200

    imported DynamicCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+import DynamicCounter from './components/DynamicCounter/dynamicCounter'
 
 function App() {
-
+  return(
+    <div>
+      <DynamicCounter />
+    </div>
+  )
 }
 export default App
